c11b892fc6dd4907e2aba671b62eaec428cab621
Aneesh <aneeshpanoli@gmail.com>
2020-10-18 18:39:46 -0700

    reg update

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0fda7ebd2daae270c73fa5fdd919226e.js"
+  "/precache-manifest.787bc963eb15a47f96d94c8f34648e6c.js"
 );
 
 self.addEventListener('message', (event) => {
